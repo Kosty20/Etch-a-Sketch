fc52e90a095e4383130c2a92a21e30d433ea145a
Kosty20 <104622649+Kosty20@users.noreply.github.com>
2023-01-05 12:23:32 +0200

    Nearly done

--- code.js
+++ code.js
@@ -1,35 +1,44 @@
 const board = document.querySelector('#container');
+let bits = [];
 const colorInput = document.querySelector('#color');
-const clearBtn = document.querySelector('.clear');
-const rainbowBtn = document.querySelector('.rainbow');
 const sizeSlider = document.querySelector('#size');
 const sizeLabel = document.querySelector('.grid-size');
-let bits = [];
+
+const clearBtn = document.querySelector('.clear');
+const rainbowBtn = document.querySelector('.enable-rainbow');
+const shadeBtn = document.querySelector('.enable-shade');
+const colorBtn = document.querySelector('.enable-color');
 
 let gridSize = sizeSlider.value;
-let color = colorInput.value;
-
-
-addBit(gridSize);
-
-
 sizeSlider.addEventListener('input', (e) => {
     sizeLabel.innerText = `${e.target.value} x ${e.target.value}`;
     gridSize = e.target.value;
     addBit(gridSize);
 });
 
-
+let color = colorInput.value;
 colorInput.addEventListener('input', (e) => {
     color = e.target.value;
-})
+});
+
+let pen = 'color';
+colorBtn.onclick = () => {pen = 'color'};
+shadeBtn.onclick = () => {pen = 'shade'};
+rainbowBtn.onclick = () => {pen = 'rainbow'}
+
+let mouseDown = false;
+document.body.onmousedown = () => {mouseDown = true};
+document.body.onmouseup = () => {mouseDown = false};
+
+
+addBit(gridSize);
 
 
 function addBit(size) {
     deleteBits();
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    for (let i = 1; i <= size ** 2; i++) {
+    for (let i = 1; i <= size * size; i++) {
         container.append(document.createElement('div'));
     }
     bits = document.querySelectorAll('#container>div');//gathers all the bits in a variable
@@ -46,16 +55,25 @@ function deleteBits(){
 
 function colorBit(){
     bits.forEach(bit => {
+        let brgt = 100;
         bit.addEventListener('mouseover', () => {
-            bit.style.backgroundColor = `${rainbowBits()}`;
-        });
+            if(mouseDown){
+                if(pen === 'color'){
+                    bit.style.backgroundColor = color;
+                } else if(pen === 'shade'){
+                    brgt = brgt - 10;
+                    bit.style.filter = `brightness(${brgt}%)`;
+                } else if(pen === 'rainbow'){
+                    bit.style.backgroundColor = `${rainbowBits()}`;
+                }
+            }
+        })
     });
 }
 
+
 function clearBits() {
-    for(bit of bits){
-        bit.style.backgroundColor = 'white';
-    }
+    addBit(gridSize);
 }
 
 
